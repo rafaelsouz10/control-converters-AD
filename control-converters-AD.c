08f7a3af58407b25e6757741c4e4873a06a770b6
Rafael Souza Barbosa <rafaelsouz10@gmail.com>
2025-02-12 12:05:36 -0300

    controle de intensidade de leds RGB adicionado

--- control-converters-AD.c
+++ control-converters-AD.c
@@ -1,14 +1,17 @@
-/*
-   Exemplo 4 do EBook pelo prof. Pedro Henrique Almeida Miranda
-*/
-
 #include <stdio.h>            
 #include "pico/stdlib.h"      
 #include "hardware/adc.h"     
 #include "hardware/pwm.h"     
 
-#define VRX_PIN 26  
-#define LED_PIN 13  
+#define VRX_PIN_RED 26
+#define VRX_PIN_BLUE 27
+#define LED_PIN_RED 13  
+#define LED_PIN_BLUE 12
+
+#define ADC_CENTER 2150
+
+float duty_cycle_red;
+float duty_cycle_blue;
 
 uint pwm_init_gpio(uint gpio, uint wrap) {
     gpio_set_function(gpio, GPIO_FUNC_PWM);
@@ -26,31 +29,39 @@ int main() {
 
     adc_init(); 
 
-    adc_gpio_init(VRX_PIN); 
+    adc_gpio_init(VRX_PIN_RED);
+    adc_gpio_init(VRX_PIN_BLUE);
 
-    uint pwm_wrap = 4096;  
-    uint pwm_slice = pwm_init_gpio(LED_PIN, pwm_wrap);  
+    uint pwm_wrap = 4095;  
+    uint pwm_slice_red = pwm_init_gpio(LED_PIN_RED, pwm_wrap);
+    uint pwm_slice_blue = pwm_init_gpio(LED_PIN_BLUE, pwm_wrap);
     
-    uint32_t last_print_time = 0; 
+    
+    uint32_t last_print_time = 0;
 
     while (true) {
+        adc_select_input(1);  
+        uint16_t vrx_value = adc_read();
         adc_select_input(0);  
-        uint16_t vrx_value = adc_read(); 
+        uint16_t vry_value = adc_read();
 
-        pwm_set_gpio_level(LED_PIN, vrx_value); 
-
-        float duty_cycle = (vrx_value / 4095.0) * 100;  
-
+        if(vrx_value <= ADC_CENTER) pwm_set_gpio_level(LED_PIN_RED, 0);
+        else pwm_set_gpio_level(LED_PIN_RED, vrx_value);
         
+        if (vry_value <= ADC_CENTER) pwm_set_gpio_level(LED_PIN_BLUE, 0);
+        else pwm_set_gpio_level(LED_PIN_BLUE, vry_value);
+
+        duty_cycle_red = (vrx_value / 4095.0) * 100;
+        duty_cycle_blue = (vry_value / 4095.0) * 100;
+          
+
         uint32_t current_time = to_ms_since_boot(get_absolute_time());  
         if (current_time - last_print_time >= 1000) {  
-            printf("VRX: %u\n", vrx_value); 
-            printf("Duty Cycle LED: %.2f%%\n", duty_cycle); 
+            printf("VRX: %u, Duty Cycle RED: %.2f%%\n", vrx_value, duty_cycle_red);
+            printf("VRY: %u, Duty Cycle BLUE: %.2f%%\n\n", vry_value, duty_cycle_blue);
             last_print_time = current_time;  
         }
-
         sleep_ms(100);  
     }
-
     return 0;  
 }
